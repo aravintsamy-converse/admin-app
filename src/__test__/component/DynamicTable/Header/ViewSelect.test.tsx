ad5f66f0d819dd698e86faac4ee91ff097a606fb
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2025-06-19 14:12:47 +0530

    test case are refactored

--- src/__test__/component/DynamicTable/Header/ViewSelect.test.tsx
+++ src/__test__/component/DynamicTable/Header/ViewSelect.test.tsx
@@ -26,168 +26,72 @@ const mockViewOptions: TableMetadata["view_options"] = [
   },
 ];
 
+const setup = (overrides = {}) => {
+  const props = {
+    viewOptions: mockViewOptions,
+    selectedView: mockViewOptions[0].value,
+    defaultView: mockViewOptions[0].value,
+    onSelectedViewChange: jest.fn(),
+    onDefaultViewChange: jest.fn(),
+    onOpenChange: jest.fn(),
+    open: true,
+    ...overrides,
+  };
+
+  const utils = render(<ViewSelect {...props} />);
+  return {
+    ...utils,
+    ...props,
+    user: userEvent.setup(),
+  };
+};
+
 describe('ViewSelect Component', () => {
   it('renders correctly with selected view label', () => {
-    const mockSelectedView = mockViewOptions[0].value;
-    const onSelectedViewChange = jest.fn();
-    const onDefaultViewChange = jest.fn();
-    const onOpenChange = jest.fn();
-
-    render(
-      <ViewSelect
-        viewOptions={mockViewOptions}
-        selectedView={mockSelectedView}
-        onSelectedViewChange={onSelectedViewChange}
-        defaultView={mockViewOptions[0].value}
-        onDefaultViewChange={onDefaultViewChange}
-        open={false}
-        onOpenChange={onOpenChange}
-      />
-    );
-
+    setup({ open: false });
     expect(screen.getByText('Default')).toBeInTheDocument();
   });
 
   it('calls onSelectedViewChange when a different view is selected', async () => {
-    const user = userEvent.setup();
-    const onSelectedViewChange = jest.fn();
-    const onDefaultViewChange = jest.fn();
-    const onOpenChange = jest.fn();
-
-    render(
-      <ViewSelect
-        viewOptions={mockViewOptions}
-        selectedView={mockViewOptions[0].value}
-        onSelectedViewChange={onSelectedViewChange}
-        defaultView={mockViewOptions[0].value}
-        onDefaultViewChange={onDefaultViewChange}
-        open={true}
-        onOpenChange={onOpenChange}
-      />
-    );
-
+    const { user, onSelectedViewChange } = setup();
     const newLabel = mockViewOptions[1].label;
     const newItem = await screen.findByText(newLabel);
 
     await user.click(newItem);
-
     expect(onSelectedViewChange).toHaveBeenCalledWith(mockViewOptions[1].value);
   });
-});
-
-describe('ViewSelect Component', () => {
-
 
   it('does NOT open dialog when clicking pin on current default view', async () => {
-    const user = userEvent.setup();
-    const onSelectedViewChange = jest.fn();
-    const onDefaultViewChange = jest.fn();
-    const onOpenChange = jest.fn();
-
-    render(
-      <ViewSelect
-        viewOptions={mockViewOptions}
-        selectedView={mockViewOptions[0].value}
-        onSelectedViewChange={onSelectedViewChange}
-        defaultView={mockViewOptions[0].value}
-        onDefaultViewChange={onDefaultViewChange}
-        open={true}
-        onOpenChange={onOpenChange}
-      />
-    );
-
-    const pinButton = screen.getByTestId(`pin-icon-${mockViewOptions[0].value}`);
-    await user.click(pinButton);
-
-    // Expect the dialog NOT to open (text won't be present)
+    const { user } = setup();
+    const pin = screen.getByTestId(`pin-icon-${mockViewOptions[0].value}`);
+    await user.click(pin);
     expect(screen.queryByText(/Do you really want to change your default list to/i)).not.toBeInTheDocument();
   });
 
   it('opens confirmation dialog when pin icon of non-default view is clicked', async () => {
-    const user = userEvent.setup();
-    const onSelectedViewChange = jest.fn();
-    const onDefaultViewChange = jest.fn();
-    const onOpenChange = jest.fn();
+    const { user, onDefaultViewChange } = setup();
+    const pin = screen.getByTestId(`pin-icon-${mockViewOptions[1].value}`);
+    await user.click(pin);
 
-    render(
-      <ViewSelect
-        viewOptions={mockViewOptions}
-        selectedView={mockViewOptions[0].value}
-        onSelectedViewChange={onSelectedViewChange}
-        defaultView={mockViewOptions[0].value} // Default is first item
-        onDefaultViewChange={onDefaultViewChange}
-        open={true}
-        onOpenChange={onOpenChange}
-      />
-    );
-
-    // Click on the pin icon of the *non-default* view
-    const pinButton = screen.getByTestId(`pin-icon-${mockViewOptions[1].value}`);
-    await user.click(pinButton);
-
-    // Confirm that the dialog appears and includes the label of the selected pending view
     expect(await screen.findByText(/Do you really want to change your default list to/i)).toBeInTheDocument();
 
-    const setDefaultButton = screen.getByText(/Set default/i);
-    const cancelButton = screen.getByText(/Cancel/i);
-
-    await user.click(setDefaultButton);
+    await user.click(screen.getByText(/Set Default/i));
     expect(onDefaultViewChange).toHaveBeenCalledWith(mockViewOptions[1].value);
-
-    await user.click(cancelButton);
-    expect(screen.queryByText(/Do you really want to change your default list to/i)).not.toBeInTheDocument();
   });
 
-  it('opens confirmation dialog when pin icon of non-default view is clicked and cancel is clicked', async () => {
-    const user = userEvent.setup();
-    const onSelectedViewChange = jest.fn();
-    const onDefaultViewChange = jest.fn();
-    const onOpenChange = jest.fn();
+  it('closes confirmation dialog when Cancel is clicked', async () => {
+    const { user } = setup();
+    const pin = screen.getByTestId(`pin-icon-${mockViewOptions[1].value}`);
+    await user.click(pin);
 
-    render(
-      <ViewSelect
-        viewOptions={mockViewOptions}
-        selectedView={mockViewOptions[0].value}
-        onSelectedViewChange={onSelectedViewChange}
-        defaultView={mockViewOptions[0].value} // Default is first item
-        onDefaultViewChange={onDefaultViewChange}
-        open={true}
-        onOpenChange={onOpenChange}
-      />
-    );
-
-    // Click on the pin icon of the *non-default* view
-    const pinButton = screen.getByTestId(`pin-icon-${mockViewOptions[1].value}`);
-    await user.click(pinButton);
-
-    // Confirm that the dialog appears and includes the label of the selected pending view
     expect(await screen.findByText(/Do you really want to change your default list to/i)).toBeInTheDocument();
-
-    const cancelButton = screen.getByText(/Cancel/i);
-
-    await user.click(cancelButton);
+    await user.click(screen.getByText(/Cancel/i));
     expect(screen.queryByText(/Do you really want to change your default list to/i)).not.toBeInTheDocument();
   });
 
   it('falls back to selectedView value if label is not found in viewOptions', () => {
-    const onSelectedViewChange = jest.fn();
-    const onDefaultViewChange = jest.fn();
-    const onOpenChange = jest.fn();
-
     const unknownView = 'unknown-view-id';
-
-    render(
-      <ViewSelect
-        viewOptions={mockViewOptions}
-        selectedView={unknownView}
-        onSelectedViewChange={onSelectedViewChange}
-        defaultView={mockViewOptions[0].value}
-        onDefaultViewChange={onDefaultViewChange}
-        open={false}
-        onOpenChange={onOpenChange}
-      />
-    );
-    // Fallback to showing the raw selectedView string
+    setup({ selectedView: unknownView, open: false });
     expect(screen.getByText('Select a view')).toBeInTheDocument();
   });
 });
